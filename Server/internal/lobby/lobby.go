b172fbc28e954953b1bf6d46379e2104a77c730d
BersI <bersivd@icloud.com>
2024-01-15 05:57:39 +0300

    Closes #37

---
+++ Server/internal/lobby/lobby.go
@@ -0,0 +1,27 @@
+package lobby
+
+import (
+	"context"
+)
+
+type Lobby struct {
+	Id            int64  `json:"id"`
+	LobbyMasterID int64  `json:"lobbyMasterID"`
+	LobbyName     string `json:"lobbyName"`
+	LobbyPassword string `json:"lobbyPassword"`
+	Amount        int64  `json:"amount"`
+}
+
+type GetLobbyRes struct {
+	Id          int64  `json:"id"`
+	LobbyName   string `json:"lobbyName"`
+	LobbyMaster string `json:"lobbyMaster"`
+}
+
+type Repository interface {
+	GetAllLobby(ctx context.Context) ([]GetLobbyRes, error)
+}
+
+type Service interface {
+	GetAllLobby(c context.Context) ([]GetLobbyRes, error)
+}
