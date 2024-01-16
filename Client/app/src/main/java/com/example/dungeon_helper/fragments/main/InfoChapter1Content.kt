8a86df266a8914c27953e05c701a2ac11bec7ffb
Dasha <dasaastafeva429@gmail.com>
2024-01-16 17:11:24 +0300

    Update and add info fragments

--- Client/app/src/main/java/com/example/dungeon_helper/fragments/main/InfoChapter1Content.kt
+++ Client/app/src/main/java/com/example/dungeon_helper/fragments/main/InfoChapter1Content.kt
@@ -8,24 +8,24 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.dungeon_helper.R
 
-class InfoChapter1 : Fragment() {
+class InfoChapter1Content : Fragment() {
 
     companion object {
-        fun newInstance() = InfoChapter1()
+        fun newInstance() = InfoChapter1Content()
     }
 
-    private lateinit var viewModel: InfoChapter1ViewModel
+    private lateinit var viewModel: InfoChapter1ContentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_info_chapter1, container, false)
+        return inflater.inflate(R.layout.fragment_info_chapter1_content, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(InfoChapter1ViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(InfoChapter1ContentViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
